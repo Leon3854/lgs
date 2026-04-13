88edad0b35b924800f8a2cee6184417658a33475
Leoned <frad109@mail.ru>
2026-04-13 20:16:55 +0300

    feat(obs): enable prometheus metrics and structured metrics endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Это магическая строка, которая создает путь /metrics
+    PrometheusModule.register({
+      path: '/metrics',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
